6a0b3501d83270dd0ff571a2bdf228f7ceb4f7d3
uzanenis <uzanenis@outlook.com>
2023-01-10 20:06:16 +0300

    homepage router fix

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
 import Button from "../components/Button";
-import { useState } from "react";
-import LoginCard from "../components/LoginCard";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 function HomePage({ ...props }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
-  return (
-    <>
-      {isLoggedIn ? (
-        <div>
-          {isLoggedIn}
-          <Button>Save</Button>
-        </div>
-      ) : (
-        router.push("/login")
-      )}
-    </>
-  );
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+  }, [isLoggedIn]);
+  return <div>Loading</div>;
 }
 
 export default HomePage;
