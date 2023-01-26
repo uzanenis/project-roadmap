60ea968659da1691df15643057ee8824dee8412a
uzanenis <uzanenis@outlook.com>
2023-01-26 09:03:43 +0300

    removed redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
 import "../styles/app.scss";
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Provider store={store}>
+      <Head>
+        <title>Project Roadmap</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
         <Component {...pageProps} />
-      </Provider>
+      </MantineProvider>
     </>
   );
 }
--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./userSlice";
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
